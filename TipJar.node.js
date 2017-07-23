51242124bf3e01f3a416ead3dcd0aef334263c92
Blair Neumann <blairneumann@plasmatechpartners.com>
2017-07-23 02:37:54 -0700

    Lambda Node.js 6.10 SDK

--- TipJar.node.js
+++ TipJar.node.js
@@ -1,36 +1,76 @@
 'use strict';
 
 var AWS = require('aws-sdk');
-var SES = new AWS.SES();
+var SES = new AWS.SES({
+    region: 'us-west-2'
+});
 
 var SOURCE = process.env.SOURCE;
 var DESTINATION = process.env.DESTINATION;
 
-exports.handler = function (event, context) {
-    sendEmail(event, function (err, data) {
-        context.done(err, null);
-    });
-};
- 
-function sendEmail (event, done) {
+var maxTipLength = 140;
+
+function done(callback, error, result) {
+    if (error) {
+        console.log('Error: '+ error);
+    } else {
+        console.log('Success: '+ result);
+    }
+
+    callback(error, result);
+}
+
+exports.handler = function (event, context, callback) {
+    console.log('Event: ', JSON.stringify(event));
+
+    // context.callbackWaitsForEmptyEventLoop = false; 
+
+    if (!event.tip) {
+        done(callback, 'Bad Request: event.tip is missing', null);
+        return;
+    }
+    if ('string' != typeof event.tip) {
+        done(callback, 'Bad Request: typeof event.tip is not string: '+
+            (typeof event.tip), null);
+        return;
+    }
+    if (event.tip.length > maxTipLength) {
+        done(callback, 'Bad Request: event.tip is longer than maxTipLength: '+ 
+            maxTipLength, null);
+        return;
+    }
+
     var params = {
         Source: SOURCE,
         Destination: {
             ToAddresses: [ DESTINATION ]
         },
         Message: {
+            Subject: {
+                Charset: 'UTF-8',
+                Data: 'Tip Jar: You have a new tip in the Tip Jar!'
+            },
             Body: {
                 Text: {
                     Charset: 'UTF-8',
                     Data: 'Tip: ' + event.tip
                 }
-            },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: 'Tip Jar: You have a new tip in the Tip Jar!'
             }
         }
     };
     
-    SES.sendEmail(params, done);
-}
+    console.log('Send Mail');
+
+    var email = SES.sendEmail(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+
+            done(callback, 'Send Mail', null);
+        } else {
+            console.log('Data: ', data);
+            console.log('Email: ', email);
+            
+            done(callback, null, 'Send Mail');
+        }
+    });
+};
