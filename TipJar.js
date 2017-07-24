ae4ca86aef7e26e955041b51e88a3ffd629159a3
Blair Neumann <blairneumann@plasmatechpartners.com>
2017-07-24 09:23:03 -0700

    Refactored to support unit tests

--- TipJar.js
+++ TipJar.js
@@ -1,5 +1,8 @@
+// The maximum length of a tip.
 var maxCharCount = 140;
 
+// A list of banal placeholder tips to help liven up the UI.
+// We'll show random ones as placeholder text in our input field.
 var listOfPlaceholderTips = [
     "Don't run with scissors.",
     "Look both ways before crossing the street.",
@@ -13,62 +16,76 @@ var listOfPlaceholderTips = [
     "Always treat others the way you want to be treated.",
 ];
 
-function buildAlertString (alertClass, alertContent) {
-    return " \
+// The HTML for our alert elements, parameterized.
+var baseAlertString = " \
         <div class='container $class$'> \
             <div class='row'> \
                 <div class='col text-center align-self-center py-3'> \
                     $content$ \
                 </div> \
             </div> \
-        </div>".replace("$class$", alertClass).replace("$content$", alertContent);
+        </div>";
+
+// The HTML for our alert elements, with parameters replaced.
+function buildAlertString (alertClass, alertContent) {
+    return baseAlertString.replace('$class$', alertClass).replace('$content$', alertContent);
 }
 
-var $alertSuccess = $(buildAlertString("alert-success",
-    "<h2>Thanks for the tip!</h2>"));
-var $alertError = $(buildAlertString("alert-error",
-    "<h2>Uh-oh, something went wrong. :-(</h2><h2>Please try again.</h2>"));
+// Our alert elements, originally detached from the DOM.
+var $alertSuccess = $(buildAlertString('alert-success',
+    '<h2>Thanks for the tip!</h2>'));
+var $alertError = $(buildAlertString('alert-error',
+    '<h2>Uh-oh, something went wrong. :-(</h2><h2>Please try again.</h2>'));
 
-$(document).ready(function() {
-    var $input = $('#input-tip');
+// Cached jQuery objects of general interest in this script.
+var $body, $input, $charCount, $progressBar;
 
-    // Setup the input field
+// Run this when the document is finished loading.
+// Factored this way for testability.
+function onReady() {
+
+    // Populate our cached jQuery objects, or use test-doubles.
+    $body = $('#body').length ? $('#body') : $('<body></body>');
+    $input = $('#input-tip').length ? $('#input-tip') : $('<input />');
+    $charCount = $('#char-count').length ? $('#char-count') : $('<div></div>');
+    $progressBar = $('#progress-bar').length ? $('#progress-bar') : $('<div></div>');
+
+    // Setup the input field.
     $input.attr('maxlength', maxCharCount);
-    doResetInput($input);
+    doResetInput();
 
-    // Update the character count UI with each keystroke
+    // Update the character count UI with each keystroke.
     $input.keyup(function(event) {
-        doCharCount($input);
+        doCharCount();
     });
 
-    // Submit on Enter
+    // Submit on Enter. This is a single-line input field, so both LF and CR should submit.
     $input.keydown(function(event) {
         if (10 == event.which || 13 == event.which) {
             doTipJar();
             return false;
         } 
     });
-});
+} $(document).ready(onReady);
 
-// Reset the input field
-function doResetInput($input) {
+// Reset the input field to its enabled state.
+function doResetInput() {
     $input.removeAttr('disabled');
-    doRandomTipPlaceholder($input);
+    doRandomTipPlaceholder();
     $input.val('');
-    doCharCount($input);
+    doCharCount();
     $input.focus();
 }
 
-// Update the random tip placeholder
-function doRandomTipPlaceholder($input) {
+// Update the random tip placeholder.
+function doRandomTipPlaceholder() {
     $input.attr('placeholder',
         listOfPlaceholderTips[Math.floor(Math.random() * listOfPlaceholderTips.length)]);
 }
 
-// Update the character count UI
-function doCharCount($input) {
-    var $charCount = $('#char-count');
-    var charCount = $input.val().length;
+// Update the character count UI.
+function doCharCount() {
+    var charCount = $input.val() ? $input.val().length : 0;
 
     $charCount.text(maxCharCount - charCount);
 
@@ -81,22 +98,22 @@ function doCharCount($input) {
     }
 }
 
-// Update the progress bar
+// Update the progress bar.
 function doProgressBar(percent) {
-    $('#progress-bar').css('width', percent +'%');
+    $progressBar.css('width', percent +'%');
 }
 
-// Update the progress bar as upload progresses
+// Update the progress bar as upload and download progress.
 function xhr() {
     var xhr = new window.XMLHttpRequest();
 
-    xhr.upload.addEventListener("progress", function(event) {
+    xhr.upload.addEventListener('progress', function(event) {
         if (event.lengthComputable) {
             doProgressBar(event.loaded / event.total * 50);
         }
     }, false);
 
-    xhr.addEventListener("progress", function(event) {
+    xhr.addEventListener('progress', function(event) {
         if (event.lengthComputable) {
             doProgressBar(50 + event.loaded / event.total * 50);
         }
@@ -105,22 +122,21 @@ function xhr() {
     return xhr;
 }
 
-// Show a UI alert
+// Show a UI alert.
 function doShowAlert($alert) {
-    var $input = $('#input-tip');
     var duration = 1000; // 1 second
 
-    $('body').append($alert);
+    $body.append($alert);
     $alert.hide();
     $alert.css('top', -1 * $(window).height() / 2 - $alert.height() / 2);
     $alert.fadeIn(duration, function() {
-        $('#progress-bar').css('width', 0);
+        $progressBar.css('width', 0);
     });
     
     $alert.delay(duration);
 
     $alert.fadeOut(duration, function() {
-        doResetInput($input);        
+        doResetInput();        
         $alert.detach();
     });
 }
@@ -137,7 +153,6 @@ function onError(xhr, status, error) {
 
 // Submit 
 function doTipJar() {
-    var $input = $('#input-tip');
     var tip = $input.val().trim();
 
     if (!tip.length) {
--- test/TipJarTest.js
+++ test/TipJarTest.js
@@ -1,11 +1,92 @@
+/*
+ * Note: PhantomJS is being used to run these tests from the command line, and
+ * PhantomJS doesn't support ES6 features yet, so this is implemented to ES5.
+ * For example, using 'var' where 'let' might make more sense.
+ */ 
+
 var assert = chai.assert;
 
-describe('TipJar', function() {
-  it('should have a Success alert element', function () {
-    assert.exists($alertSuccess, '$alertSuccess exists');
+describe('maxCharCount', function () {
+  it('should be a positive integer', function () {
+    assert.isNumber(maxCharCount, 'is a number');
+    assert.isAbove(maxCharCount, 0, 'is positive');
+    assert.strictEqual(maxCharCount - Math.floor(maxCharCount), 0, 'is an integer');
   });
+});
 
-  it('should have an Error alert element', function () {
-    assert.exists($alertError, '$alertError exists');
+describe('listOfPlaceholderTips', function () {
+  it('should be a non-trivial array of strings', function () {
+    assert.isArray(listOfPlaceholderTips, 'is an array');
+    assert.isAbove(listOfPlaceholderTips.length, 0, 'is a non-trivial array');
+    for (var idx = 0; idx < listOfPlaceholderTips.length; ++idx) {
+      assert.isString(listOfPlaceholderTips[idx], 'tip is a string');
+    }
   });
+});
+
+var alertStringParams = ['$class$', '$content$'];
+
+describe('baseAlertString', function () {
+  it('should be a non-trivial string', function () {
+    assert.isString(baseAlertString, 'is a string');
+    assert.isAbove(baseAlertString.length, 0, 'is a non-trivial string');
+  });
+  it('should be parameterized', function () {
+    for (var idx = 0; idx < alertStringParams.length; ++idx) {
+      assert.isAbove(baseAlertString.indexOf(alertStringParams[idx]), -1,
+        'contains parameter '+ alertStringParams[idx]);
+    }
+  });
+});
+
+describe('buildAlertString', function () {
+  var alert = buildAlertString('test-class', 'test-content');
+
+  it('should return a non-trivial string', function () {
+    assert.isString(alert, 'returns a string');
+    assert.isAbove(alert.length, 0, 'returns a non-trivial string');
+  });
+  it('should have its parameters replaced', function () {
+    for (var idx = 0; idx < alertStringParams.length; ++idx) {
+      assert.strictEqual(alert.indexOf(alertStringParams[idx]), -1,
+        'parameter is replaced: '+ alertStringParams[idx]);
+    }
+  });
+});
+
+function testElement (name, element) {
+  it(name +' should exist', function () {
+    assert.exists(element, 'exists');
+  });
+  it(name +' should be HTML', function () {
+    assert.isString(element.innerHTML, 'inner HTML is string');
+  });  
+}
+
+function testAlertElement (name, element) {
+  describe('Alert Element: '+ name, function () {
+    testElement(name, element);
+    it('should contain inner text', function () {
+      assert.isString(element.innerText, 'inner text is string');
+      assert.isString(element.innerText.trim(), 'inner text is non-trivial string');
+    });
+    it('should have its parameters replaced', function () {
+      for (var idx = 0; idx < alertStringParams.length; ++idx) {
+        assert.strictEqual(element.innerHTML.indexOf(alertStringParams[idx]), -1,
+          'parameter is replaced: '+ alertStringParams[idx]);
+      }
+    });
+  });
+}
+
+testAlertElement('Success', $alertSuccess[0]);
+testAlertElement('Error', $alertError[0]);
+
+onReady();
+
+describe('onReady', function () {
+  testElement("$body", $body[0]);
+  testElement("$input", $input[0]);
+  testElement("$progressBar", $progressBar[0]);
+  testElement("$charCount", $charCount[0]);
 });
