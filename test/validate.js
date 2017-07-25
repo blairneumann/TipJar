1255bf69d35e3846b97507ef1a64d419b052b3d6
Blair Neumann <blairneumann@plasmatechpartners.com>
2017-07-24 21:27:00 -0700

    Refactor linting

--- test/validate.js
+++ test/validate.js
@@ -1,9 +1,10 @@
 /* eslint no-throw-literal: "off" */
+/* eslint-env node */
 
 var w3cjs = require('w3cjs');
 
 describe('HTML validation', function() {
-  it('index page should have no HTML errors', function(done) {
+  it('index.html should have no validation errors', function(done) {
     w3cjs.validate({
       file: 'index.html',
       callback: function(error, res) {
@@ -15,7 +16,7 @@ describe('HTML validation', function() {
     });
   });
 
-  it('error page should have no HTML errors', function(done) {
+  it('error.html should have no validation errors', function(done) {
     w3cjs.validate({
       file: 'error.html',
       callback: function(error, res) {
